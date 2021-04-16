21a42effa627733b0ec00cb8629b5499e5d290f5
halmetov <bahti_2015@list.ru>
2021-04-16 13:50:33 +0600

    'fix'

--- main/views.py
+++ main/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from main.models import News
+from datetime import datetime
 
 
 # Create your views here.
@@ -10,23 +11,54 @@ def indexHandler(request):
     section = request.GET.get('section', '')
     page_size = int(request.GET.get('page-size', 10))
     orderby = request.GET.get('order-by', 'newest')
+    from_date = request.GET.get('from-date', None) #2021-04-14
+
+    if from_date:
+        from_date = datetime.strptime(from_date, '%Y-%m-%d')
 
     if section:
         if orderby == 'newest':
-            news = News.objects.filter(category__title=section).order_by('-date')[0:page_size]
+            if from_date:
+                news = News.objects.filter(category__title=section).filter(date__gte=from_date).order_by('-date')[0:page_size]
+            else:
+                news = News.objects.filter(category__title=section).order_by('-date')[0:page_size]
+
         elif orderby == 'oldest':
-            news = News.objects.filter(category__title=section).order_by('date')[0:page_size]
+            if from_date:
+                news = News.objects.filter(category__title=section).filter(date__gte=from_date).order_by('date')[0:page_size]
+            else:
+                news = News.objects.filter(category__title=section).order_by('date')[0:page_size]
+
         elif orderby == 'relevance':
-            news = News.objects.filter(category__title=section).filter(status=0).order_by('-date')[0:page_size]
+            if from_date:
+                news = News.objects.filter(category__title=section).filter(status=0).filter(date__gte=from_date).order_by('-date')[0:page_size]
+            else:
+                news = News.objects.filter(category__title=section).filter(status=0).order_by('-date')[0:page_size]
+
         else:
-            news = News.objects.filter(category__title=section)[0:page_size]
+            if from_date:
+                news = News.objects.filter(category__title=section).filter(date__gte=from_date)[0:page_size]
+            else:
+                news = News.objects.filter(category__title=section)[0:page_size]
     else:
         if orderby == 'newest':
-            news = News.objects.all().order_by('-date')[0:page_size]
+            if from_date:
+                news = News.objects.all().filter(date__gte=from_date).order_by('-date')[0:page_size]
+            else:
+                news = News.objects.all().order_by('-date')[0:page_size]
+
         elif orderby == 'oldest':
-            news = News.objects.all().order_by('date')[0:page_size]
+            if from_date:
+                news = News.objects.all().filter(date__gte=from_date).order_by('date')[0:page_size]
+            else:
+                news = News.objects.all().order_by('date')[0:page_size]
+
         elif orderby == 'relevance':
-            news = News.objects.filter(status=0).order_by('-date')[0:page_size]
+            if from_date:
+                news = News.objects.filter(status=0).filter(date__gte=from_date).order_by('-date')[0:page_size]
+            else:
+                news = News.objects.filter(status=0).order_by('-date')[0:page_size]
+
         else:
             news = News.objects.all()[0:page_size]
     new_news = []
